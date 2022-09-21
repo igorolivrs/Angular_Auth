5e0c2dd8c61d303f708158bc49edbf122d6dd8f0
Igor Oliveira <igorsouza.psn@gmail.com>
2022-09-21 22:08:34 +0100

    Login and SignUp API .Net

---
+++ Angular_AuthAPI/Angular_AuthAPI/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Angular_AuthAPI.Context;
+using Angular_AuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angular_AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.UserName == userObj.UserName && x.Password == userObj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            return Ok(new
+            {
+                Message = "Login Sucess!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
